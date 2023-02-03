38bc7eed68a06573f4d58f544e0bdcd11d7aaaec
Sivakrishna29 <sivakrishnakasaragadda@gmail.com>
2023-02-03 16:58:51 +0530

    Assignment11 - State lifing changes

--- src/Components/Instamart.js
+++ src/Components/Instamart.js
@@ -2,12 +2,11 @@
 // useState = react.useState;
 import { useState } from "react";
 
-const Section = ({ name, description }) => {
-  const [isVisible, setIsVisible] = useState(true);
+const Section = ({ name, description, isVisible, setIsVisible }) => {
   return (
     <div className="border border-black p-3 m-3">
       <h1 className="font-semibold text-2xl">{name}</h1>
-      {isVisible ? (
+      {!isVisible ? (
         <button className="underline" onClick={() => setIsVisible(false)}>
           Show
         </button>
@@ -22,19 +21,34 @@ const Section = ({ name, description }) => {
 };
 
 const Instamart = () => {
+  const [visibleSection, setIsVisibleSection] = useState("");
+
   return (
     <div className="pl-3 pb-3">
+      <h1 className="font-bold text-2xl pl-2">Instamart</h1>
       <Section
         name="About instamart"
         description="On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through weakness of will, which is the same as saying through shrinking from toil and pain. These cases are perfectly simple and easy to distinguish. In a free hour, when our power of choice is untrammelled and when nothing prevents our being able to do what we like best, every pleasure is to be welcomed and every pain avoided. But in certain circumstances and owing to the claims of duty or the obligations of business it will frequently occur that pleasures have to be repudiated and annoyances accepted. The wise man therefore always holds in these matters to this principle of selection: he rejects pleasures to secure other greater pleasures, or else he endures pains to avoid worse pains."
+        isVisible={visibleSection === "about"}
+        setIsVisible={() =>
+          setIsVisibleSection(visibleSection === "about" ? "" : "about")
+        }
       />
       <Section
         name="Team instamart"
         description="On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through weakness of will, which is the same as saying through shrinking from toil and pain. These cases are perfectly simple and easy to distinguish. In a free hour, when our power of choice is untrammelled and when nothing prevents our being able to do what we like best, every pleasure is to be welcomed and every pain avoided. But in certain circumstances and owing to the claims of duty or the obligations of business it will frequently occur that pleasures have to be repudiated and annoyances accepted. The wise man therefore always holds in these matters to this principle of selection: he rejects pleasures to secure other greater pleasures, or else he endures pains to avoid worse pains."
+        isVisible={visibleSection === "team"}
+        setIsVisible={() =>
+          setIsVisibleSection(visibleSection === "team" ? "" : "team")
+        }
       />
       <Section
         name="Careers"
         description="On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through weakness of will, which is the same as saying through shrinking from toil and pain. These cases are perfectly simple and easy to distinguish. In a free hour, when our power of choice is untrammelled and when nothing prevents our being able to do what we like best, every pleasure is to be welcomed and every pain avoided. But in certain circumstances and owing to the claims of duty or the obligations of business it will frequently occur that pleasures have to be repudiated and annoyances accepted. The wise man therefore always holds in these matters to this principle of selection: he rejects pleasures to secure other greater pleasures, or else he endures pains to avoid worse pains."
+        isVisible={visibleSection === "career"}
+        setIsVisible={() =>
+          setIsVisibleSection(visibleSection === "career" ? "" : "career")
+        }
       />
     </div>
   );
